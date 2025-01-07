c0fa8578962b56faeb207193caef8d2cbe0ed045
James Whitaker <james@altrion.org>
2025-01-07 21:34:39 +0000

    Fix text field in model, add starting catalogue data

--- catalogue/models.py
+++ catalogue/models.py
@@ -5,15 +5,15 @@ from django.contrib.auth.models import User
 class Catalogue(models.Model):
     baseid = models.CharField(max_length=6)
     name = models.CharField(max_length=255)
-    notes = models.TextField
+    notes = models.TextField(null=True, blank=True)
 
 class PaintScheme(models.Model):
-    name = models.CharField(max_length=255)
-    notes = models.TextField
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    notes = models.TextField(null=True, blank=True)
 
 class Collection(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     mini = models.ForeignKey(Catalogue, on_delete=models.CASCADE)
     paint = models.ForeignKey(PaintScheme, blank=True, null=True, on_delete=models.CASCADE)
-    notes = models.TextField
+    notes = models.TextField(null=True, blank=True)
