247de5593987b965992ef02f55a47226ec003749
James Whitaker <james@altrion.org>
2025-01-11 00:50:19 +0000

    Fix STATIC_ROOT

--- BTCM/settings.py
+++ BTCM/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'productionfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
