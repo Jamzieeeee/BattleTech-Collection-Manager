7d1dfd014b7ddeb1d537679295b8eb70b55b9fa3
Jamzieeeee <james@altrion.org>
2025-03-13 14:37:58 +0000

    Register models in admin file

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Catalogue, Collection, PaintScheme
 
 # Register your models here.
+admin.site.register(Catalogue)
+admin.site.register(Collection)
+admin.site.register(PaintScheme)
